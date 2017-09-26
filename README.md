506d9be58322803ea4993855399b075076f80cdb
Courtney Radford <Courtney6554@Courtneys-MacBook-Pro.local>
2017-09-25 23:37:57 -0700

    t/s jquery/node int

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FriendFinder
-Find your new best friend through a series of questions!
+
+* Description: Basic app demonstrating Node and Express with jQuery. Overall purpose is to help schedule reservation requests. Restaurant has just 5 tables available. First five requests get a reservation, every request after that is sent to the waiting list.
+* Live Demo: <https://hot-restaurant.herokuapp.com/>
+
+
+## To Install
+
+* Git Clone the repository
+* Navigate to the folder where the repository exists using Git Bash or Terminal
+* Run the command `npm install` to download the required dependencies
+* Then run the command `node friends.js` to run the program
---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var friendsArray = [
+    {
+        "name":"Joe",
+        "photo":"/Images/CHC.jpg",
+        "scores":[3, 3, 5, 4, 5, 2, 2, 1, 4, 1]
+    },
+    {
+        "name":"Terry",
+        "photo":"/Images/CLE.jpg",
+        "scores":[3, 3, 5, 4, 5, 2, 2, 1, 4, 1]
+    },
+    {
+        "name":"John",
+        "photo":"/Images/BOS.jpg",
+        "scores":[3, 3, 5, 4, 5, 2, 2, 1, 4, 1]
+    },
+    {
+        "name":"Dave",
+        "photo":"/Images/LAD.jpg",
+        "scores":[3, 3, 5, 4, 5, 2, 2, 1, 4, 1]
+    },
+    {
+        "name":"Mike",
+        "photo":"/Images/STL.jpg",
+        "scores": [3, 3, 5, 4, 5, 2, 2, 1, 4, 1]
+    }
+];
+
+module.exports = friendsArray;
+
+//=======================================//
+// Can not get jquery to run with node.
+//=======================================//
+
+// var $ = require("jquery");
+// var userdata = {};
+
+// // $(document).ready(function() {
+
+// $('#submitButton').on("click", function() {
+//     if(!$('#nameEntry').val() || !$('#imageEntry').val()){
+//         alert("Complete all blanks");
+
+//     } else {
+//         userdata = 
+//         {
+//             name: $("friendName").val(),
+//             image: $("friendPhotoLink").val(),
+//             score: 
+//             [
+//                 $('#q1').val(),
+//                 $('#q2').val(),
+//                 $('#q3').val(),
+//                 $('#q4').val(),
+//                 $('#q5').val(),
+//                 $('#q6').val(),
+//                 $('#q7').val(),
+//                 $('#q8').val(),
+//                 $('#q9').val(),
+//                 $('#q10').val()           
+//             ]
+//         };
+//     console.log(userdata);
+//     }
+
+//     $.ajax({
+//         type: "GET",
+//         url: "/api/friends/",
+//         data:{ userdata: JSON.stringify(userdata) },
+//         dataType: "json"
+//     }).done(function(data){
+//         console.log(data);
+//     } );;
+    
+        
+// });
+
+ 
+
+// });
+
+// module.exports(userdata);
